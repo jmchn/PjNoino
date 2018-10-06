64fe39e958884099fd54c687ed013f1e6c76d367
Ubuntu <jianming@Onion.sov0ai3plaquzoq2obdxfeuivh.ix.internal.cloudapp.net>
2018-10-06 18:58:35 +0000

    ..

--- node/ws_server.js
+++ node/ws_server.js
@@ -1,11 +1,11 @@
 const WebSocketServer = require('ws').Server
 const ws_server = new WebSocketServer({port: 8080})
-
+console.log('sever start');
 ws_server.on('connection', function (ws, req) { // ws is the current client browser
     ws.on('message', function (message_string) {
 
         let message = JSON.parse(message_string)
-
+	console.log('message got/');
         switch (message.action) {
 
             case 'ping':
